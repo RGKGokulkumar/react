a2c013212b4397870b6876e790a235dc231646ca
RGKGokulkumar <gokulkumarrasu@gmail.com>
2022-08-07 21:21:34 +0530

    Init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Sum from "./Component/Sum";
+
+function App() {
+  return (
+    <div className="App">
+      <Sum/>
+    </div>
+  );
+}
+
+export default App;
